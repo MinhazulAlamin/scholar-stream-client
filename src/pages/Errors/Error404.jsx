eb4ecb9594430833813749a5b5ef1d96d14f9f1e
MinhazulAlamin <minhazulalamin007@gmail.com>
2025-12-19 00:33:50 +0600

    error page added

--- src/pages/Errors/Error404.jsx
+++ src/pages/Errors/Error404.jsx
@@ -1,11 +1,26 @@
 import React from 'react';
+import { useNavigate } from 'react-router';
 
 const Error404 = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+  const navigate = useNavigate();
+
+  return (
+    <div className="min-h-screen bg-gray-50 flex items-center justify-center py-12">
+      <div className="text-center px-4">
+        <h1 className="text-6xl font-bold text-red-600 mb-4">404</h1>
+        <h2 className="text-2xl font-semibold mb-2">Oops, page not found!</h2>
+        <p className="text-gray-600 mb-8">
+          The page you are looking for doesn’t exist or has been moved.
+        </p>
+        <button
+          onClick={() => navigate(-1)}
+          className="bg-green-600 text-white px-8 py-3 rounded-lg hover:bg-purple-700 transition-colors"
+        >
+          Go Back
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default Error404;
--- src/pages/Loader/Loader.jsx
+++ src/pages/Loader/Loader.jsx
@@ -1,11 +1,18 @@
 import React from 'react';
 
 const Loader = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+  return (
+    <div className="w-full h-[99vh] flex flex-col justify-center items-center bg-green-50">
+      <div className="relative w-20 h-20">
+        <div className="absolute inset-0 rounded-full border-4 border-secondary border-t-transparent animate-spin"></div>
+      </div>
+
+      <p className="mt-4 text-gray-700 text-lg font-medium animate-pulse">
+        Loading...
+      </p>
+    </div>
+  );
+  
 };
 
 export default Loader;
