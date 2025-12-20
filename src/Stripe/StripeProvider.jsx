de09a8f29ebad969591492e3aa6ccc5ba1679065
MinhazulAlamin <minhazulalamin007@gmail.com>
2025-12-20 18:24:01 +0600

    stripe relatead code added

--- src/Stripe/StripeProvider.jsx
+++ src/Stripe/StripeProvider.jsx
@@ -1,11 +1,11 @@
+import { Elements } from '@stripe/react-stripe-js';
+import { loadStripe } from '@stripe/stripe-js';
 import React from 'react';
 
-const StripeProvider = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+const stripePromise = loadStripe(import.meta.env.VITE_STRIPE_PUBLIC_KEY);
+
+const StripeProvider = ({ children }) => {
+  return <Elements stripe={stripePromise}>{children}</Elements>;
 };
 
 export default StripeProvider;
